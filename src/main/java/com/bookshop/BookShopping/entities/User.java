8d07039066d41dcc9b22d3d84bf5322dd07da277
MertColakk <meertcolak06@gmail.com>
2024-04-29 21:27:10 +0300

    User class start.

---
+++ src/main/java/com/bookshop/BookShopping/entities/User.java
@@ -0,0 +1,15 @@
+package com.bookshop.BookShopping.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter @Setter
+@AllArgsConstructor @NoArgsConstructor
+public class User {
+}
