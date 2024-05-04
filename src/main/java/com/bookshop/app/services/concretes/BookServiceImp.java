59581120e066d8c5823ba5751aaba86de9531793
Mert Çolak <quartesa@192.168.1.11>
2024-05-04 18:50:24 +0300

    login tests

---
+++ src/main/java/com/bookshop/app/services/concretes/BookServiceImp.java
@@ -0,0 +1,43 @@
+package com.bookshop.app.services.concretes;
+
+import com.bookshop.app.models.Book;
+import com.bookshop.app.repositories.BookRepository;
+import com.bookshop.app.services.abstracts.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class BookServiceImp implements BookService {
+    @Autowired
+    private BookRepository bookRepository;
+
+    @Override
+    public Book add(Book book) {
+        bookRepository.save(book);
+
+        return book;
+    }
+    @Override
+    public List<Book> show() {
+        return bookRepository.findAll();
+    }
+
+    @Override
+    public Book getWithID(long id) {
+        for(Book book:bookRepository.findAll()){
+            if(book.getId()==id){
+                return book;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public Book delete(Book book) {
+        bookRepository.delete(book);
+        return book;
+    }
+}
---
+++ src/test/java/com/bookshop/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookshop.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
